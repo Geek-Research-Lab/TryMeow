d1d70a75cd723897601e84cbd73adb89f97a28b3
Aswini S <ashumeow@live.com>
2015-01-02 06:51:52 +0530

    Create num.js

---
+++ num.js
@@ -0,0 +1,10 @@
+var Meow_Num = function() {
+	Meow_Token.Meow_Num = function() {
+		var Meow_Val;
+		var v = Meow_Val;
+		super(tag.num);
+	};
+	function toString() {
+		return "" + Meow_Val;
+	}
+};
